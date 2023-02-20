eecc6cf623c8d00105e9aa36e47dea3404292bda
RettPrattr <desaswork@gmail.com>
2023-02-20 16:34:16 +0300

    changes

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68c71afe50fa1343"; }
+/******/ 		__webpack_require__.h = function() { return "5fbaefe63cd397c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
