821762b721b1366504273eed4c9f74250f9543fa
RettPrattr <desaswork@gmail.com>
2023-02-27 14:24:17 +0300

    do

--- brokerage-strapi/src/api/form-request/controllers/form-request.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * form-request controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::form-request.form-request');
