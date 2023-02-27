8681196d21781c0be1fdad4a3784692aafa9d89c
dmitriikapustin <dmitriikapustin@ya.ru>
2023-02-27 14:18:19 +0300

    minus strapi

--- brokerage-strapi/src/api/form-request/services/form-request.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * form-request service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::form-request.form-request');
