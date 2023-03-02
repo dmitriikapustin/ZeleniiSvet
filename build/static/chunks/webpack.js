3a2fa91ab97a363b8beebd0ca32ba81283c7fffb
dmitriikapustin <dmitriikapustin@ya.ru>
2023-03-02 14:29:53 +0300

    trash

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bbf0be131c592e09"; }
+/******/ 		__webpack_require__.h = function() { return "f0e84ec4a98a4ef0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
