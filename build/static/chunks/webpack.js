04ffb0d2facd2b306114605c279a8bf7b8a563c0
dmitriikapustin <dmitriikapustin@ya.ru>
2023-03-04 14:53:11 +0300

    pull

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f0e84ec4a98a4ef0"; }
+/******/ 		__webpack_require__.h = function() { return "3dbba9f9d6dde118"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
