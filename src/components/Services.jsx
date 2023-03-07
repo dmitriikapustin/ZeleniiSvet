b2ca14dfcf6f21badba4bdb06488f7c469e83639
RettPrattr <desaswork@gmail.com>
2023-03-07 15:36:43 +0300

    services dropdown-menu in progress

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,30 +2,31 @@ import src from './../assets/png/card-mock.png'
 import srcSet from './../assets/png/card-mock@2x.png'
 import ServicesCard from './ServicesCard'
 import CardsAnimation from './animations/CardsAnimation'
+// import avto from '/images/avto.jpg'
+// import potreb from '/images/potreb.jpg'
+// import ipoteka from '/images/ipoteka.jpg'
+// import refin from '/images/refin.jpg'
+
 
 const mock = [
     {
-        src: src.src,
-        srcSet: srcSet.src,
-        title: 'Отказали в <b>кредите!</b>',
+        src: 'potreb',
+        title: 'Потребительский <b>кредит</b>',
         desc: 'Поможем! Проконсультируем и доведём до подписания договора в банке.'
     },
     {
-        src: src.src,
-        srcSet: srcSet.src,
-        title: 'Нет времени на <b>анализ</b> банков и условий',
+        src: 'refin',
+        title: 'Рефинансирование',
         desc: 'Мы всё сделаем сами, включая сбор ваших документов и одобрение кредита. А вы сэкономите свои время и силы.'
     },
     {
-        src: src.src,
-        srcSet: srcSet.src,
-        title: 'Нет официального <b>дохода</b>',
+        src: 'ipoteka',
+        title: 'Ипотека',
         desc: 'Это не проблема. Мы знаем законные способы его получить кредит, если у вас нет документально-подтверждённого дохода.'
     },
     {
-        src: src.src,
-        srcSet: srcSet.src,
-        title: 'Плохая кредитная <b>история</b> с просрочкой!',
+        src: 'avto',
+        title: 'Залог под квартиру или машину',
         desc: 'Мы повысим ваши шансы на заём без серых схем — всё прозрачно и по закону.'
     }
 ]
--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -1,11 +1,20 @@
 import { useCallback } from "react";
+import Image from 'next/image';
 
 export default function ServicesCard(props){
-    const {src, srcSet, title, desc} = props;
+    const {src, title, desc} = props;
 
     return(
         <div className="services-card">
-            <img src={src} srcSet={`${srcSet} 2x`} className="services-card__img" />
+            {/* <img src={src} srcSet={`${srcSet} 2x`} className="services-card__img" /> */}
+            <Image
+                src={'/images/' + src + '.jpg'}
+                width={150}
+                height={150}
+                quality={100}
+                alt={''}
+			>
+			</Image>
             <div className="services-card__body">
                 <div className="services-card__title" dangerouslySetInnerHTML={{__html: title}} />
                 <p className="services-card__desc">{desc}</p>
