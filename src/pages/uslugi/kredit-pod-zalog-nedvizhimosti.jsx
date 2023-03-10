d9d13eeb8610b369f9b21dbbbf3b5e749eef1894
dmitriikapustin <dmitriikapustin@ya.ru>
2023-03-11 01:36:00 +0300

    pages cleaning

--- src/pages/uslugi/kredit-pod-zalog-nedvizhimosti.jsx
+++ src/pages/uslugi/kredit-pod-zalog-nedvizhimosti.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react'
+import React from 'react'
 
 import axios from 'axios';
 
@@ -6,92 +6,70 @@ import Calculator from '@/components/Calculator';
 import Form from '@/components/Form';
 import PartnersBlock from '@/components/Partners';
 import Head from 'next/head'
-import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import MainProfits from '@/components/MainProfits'
 import Help from '@/components/Help';
 import WeWork from '@/components/WeWork';
 import MapBlock from '@/components/MapBlock';
-import Footer from '@/components/Footer';
 import CreditStory from '@/components/CreditStory';
 import Story from '@/components/Story';
 
-import {PopupState} from '@/context/buttonContext'
-
-
-export async function getStaticProps() {
-	const respPhones = await axios.get(`https://api.zesvet.ru/api/form-requests?populate=*`);
-	const dataPhonesResp = respPhones.data;
-	console.log(dataPhonesResp)
-	return { props: { dataGetPhones: dataPhonesResp } }
-}
-
-
-const kreditpodzalognedvizhimosti = ({dataGetPhones}) => {
+const kreditpodzalognedvizhimosti = () => {
   return (
     <>
-    <PopupState.Provider value={{ popupState, setPopupState }}>
-  <Head>
-    <title>Зелёный свет</title>
-    <meta property="og:title" content="Заголовок" key="title" />
-  </Head>
-  <div className='scroll-area'>
-      <Header/>
-    
-      <main>
-          <HeroSection
-            type={1}
-            newTitle='Поможем получить кредит под залог недвижимости на выгодных условиях'
-          />
+      <Head>
+        <title>Зелёный свет</title>
+        <meta property="og:title" content="Заголовок" key="title" />
+      </Head>
+      <HeroSection
+        type={1}
+        newTitle='Поможем получить кредит под залог недвижимости на выгодных условиях'
+      />
 
-        <div className='bc-white-container'>
-          <Calculator />
-          <Story 
-            type={5}
-            reverse={true}
-            title='Заголовок'
-            subtitle='Подзаголовок'
-            text='Текст'
-            photo='/images/avto.jpg'
-          />
-          <Story 
-            type={5}
-            title='Заголовок'
-            subtitle='Подзаголовок'
-            text='Текст'
-            photo='/images/avto.jpg'
-          >
-            <Button 
-                mode='light'
-                text='Бесплатная консультация'
-                icon='true'
-            />
-          </Story>
-          <Text
-            type={1}
-            title='Оформи кредит под залог недвижимости'
-            text1='Помогаем оформить имущество для использования в вашем бизнесе с последующим правом выкупа. Предложим надёжную лизинговую компанию и банк для успешной сделки.'
-            text2='Бизнес это сложная и «живая» структура, а кредиты — распространённое явление. Мы знаем, как раздобыть для вас лучший вариант займа у банка.'
-          />
-        </div>
-        
-        <Help/>
-        <MainProfits />
-        <Form
-          data={dataGetPhones}
+      <div className='bc-white-container'>
+        <Calculator />
+        <Story
+          type={5}
+          reverse={true}
+          title='Заголовок'
+          subtitle='Подзаголовок'
+          text='Текст'
+          photo='/images/avto.jpg'
         />
-        <WeWork 
-            index={3}
-            title={''}
+        <Story
+          type={5}
+          title='Заголовок'
+          subtitle='Подзаголовок'
+          text='Текст'
+          photo='/images/avto.jpg'
+        >
+          <Button
+            mode='light'
+            text='Бесплатная консультация'
+            icon='true'
+          />
+        </Story>
+        <Text
+          type={1}
+          title='Оформи кредит под залог недвижимости'
+          text1='Помогаем оформить имущество для использования в вашем бизнесе с последующим правом выкупа. Предложим надёжную лизинговую компанию и банк для успешной сделки.'
+          text2='Бизнес это сложная и «живая» структура, а кредиты — распространённое явление. Мы знаем, как раздобыть для вас лучший вариант займа у банка.'
         />
-        <CreditStory/>
-        <PartnersBlock />
-        <MapBlock/>
-      </main>
-    <Footer/>
-  </div>
-</PopupState.Provider>
-</>
+      </div>
+
+      <Help />
+      <MainProfits />
+      <Form
+        data={dataGetPhones}
+      />
+      <WeWork
+        index={3}
+        title={''}
+      />
+      <CreditStory />
+      <PartnersBlock />
+      <MapBlock />
+    </>
   )
 }
 
--- src/pages/uslugi/lizing.jsx
+++ src/pages/uslugi/lizing.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react'
+import React from 'react'
 
 import axios from 'axios';
 
@@ -6,92 +6,69 @@ import Calculator from '@/components/Calculator';
 import Form from '@/components/Form';
 import PartnersBlock from '@/components/Partners';
 import Head from 'next/head'
-import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import MainProfits from '@/components/MainProfits'
 import Help from '@/components/Help';
 import WeWork from '@/components/WeWork';
 import MapBlock from '@/components/MapBlock';
-import Footer from '@/components/Footer';
 import CreditStory from '@/components/CreditStory';
 import Story from '@/components/Story';
 
-import {PopupState} from '@/context/buttonContext'
-
-export async function getStaticProps() {
-	const respPhones = await axios.get(`https://api.zesvet.ru/api/form-requests?populate=*`);
-	const dataPhonesResp = respPhones.data;
-	console.log(dataPhonesResp)
-	return { props: { dataGetPhones: dataPhonesResp } }
-}
-
 const lizing = () => {
-  const [popupState, setPopupState] = useState(false)
-
-
   return (
     <>
-      <PopupState.Provider value={{ popupState, setPopupState }}>
-        <Head>
-          <title>Зелёный свет</title>
-          <meta property="og:title" content="Заголовок" key="title" />
-        </Head>
-        <div className='scroll-area'>
-          <Header />
+      <Head>
+        <title>Зелёный свет</title>
+        <meta property="og:title" content="Заголовок" key="title" />
+      </Head>
+      <HeroSection
+        type={1}
+        newTitle='Поможем получить лизинг на выгодных условиях'
+      />
 
-          <main>
-            <HeroSection
-              type={1}
-              newTitle='Поможем получить лизинг на выгодных условиях'
-            />
+      <div className='bc-white-container'>
+        <Calculator />
+        <Story
+          type={5}
+          reverse={true}
+          title='Заголовок'
+          subtitle='Подзаголовок'
+          text='Текст'
+          photo='/images/avto.jpg'
+        />
+        <Story
+          type={5}
+          title='Заголовок'
+          subtitle='Подзаголовок'
+          text='Текст'
+          photo='/images/avto.jpg'
+        >
+          <Button
+            mode='light'
+            text='Бесплатная консультация'
+            icon='true'
+          />
+        </Story>
+        <Text
+          type={1}
+          title='Оформи лизинг'
+          text1='Помогаем оформить имущество для использования в вашем бизнесе с последующим правом выкупа. Предложим надёжную лизинговую компанию и банк для успешной сделки.'
+          text2='Бизнес это сложная и «живая» структура, а кредиты — распространённое явление. Мы знаем, как раздобыть для вас лучший вариант займа у банка.'
+        />
+      </div>
 
-            <div className='bc-white-container'>
-              <Calculator />
-              <Story
-                type={5}
-                reverse={true}
-                title='Заголовок'
-                subtitle='Подзаголовок'
-                text='Текст'
-                photo='/images/avto.jpg'
-              />
-              <Story
-                type={5}
-                title='Заголовок'
-                subtitle='Подзаголовок'
-                text='Текст'
-                photo='/images/avto.jpg'
-              >
-                <Button
-                  mode='light'
-                  text='Бесплатная консультация'
-                  icon='true'
-                />
-              </Story>
-              <Text
-                type={1}
-                title='Оформи лизинг'
-                text1='Помогаем оформить имущество для использования в вашем бизнесе с последующим правом выкупа. Предложим надёжную лизинговую компанию и банк для успешной сделки.'
-                text2='Бизнес это сложная и «живая» структура, а кредиты — распространённое явление. Мы знаем, как раздобыть для вас лучший вариант займа у банка.'
-              />
-            </div>
-
-            <Help />
-            <MainProfits />
-            <Form
-              data={dataGetPhones}
-            />
-            <WeWork
-              index={6}
-              title={''}
-            />
-            <CreditStory />
-            <PartnersBlock />
-            <MapBlock />
-          </main>
-          <Footer />
-        </div>
-      </PopupState.Provider>
+      <Help />
+      <MainProfits />
+      <Form
+        data={dataGetPhones}
+      />
+      <WeWork
+        index={6}
+        title={''}
+      />
+      <CreditStory />
+      <PartnersBlock />
+      <MapBlock />
     </>
   )
 }
